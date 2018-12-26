63c0ad02f59bf8f1a9000d170354f47ca87e9be8
Danail Rabadzhiyski <danail@danaildr.eu>
2018-12-26 14:36:28 +0200

    Rename Bundle

--- src/SoftUProjectBundle/Controller/DefaultController.php
+++ src/SoftUProjectBundle/Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace SoftUProjectBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
